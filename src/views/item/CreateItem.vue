1ddddf42096ff66ee84f44fda0f0eda58e0d358c
multimontana <multitema@gmail.com>
2021-01-08 16:35:01 +0400

    fix

--- src/views/item/CreateItem.vue
+++ src/views/item/CreateItem.vue
@@ -1,66 +1,66 @@
 <template>
-    <div>
-        <div class="form-group">
-            <label for="">Название *</label>
-            <input type="text" class="form-control form-control-lg" v-model="item.title"
-          :class="{'is-invalid': $v.item.title.$error}"
-          @blur="$v.item.title.$touch">
-          <div class="invalid-feedback" v-if="!$v.item.title.required">
-            Title field is required
-          </div>
-        </div>
-        <div class="form-group">
-            <label for="">Наценка категории *</label>
-            <input type="text" class="form-control form-control-lg" v-model="item.markup"
-                   :class="{'is-invalid': $v.item.markup.$error}"
-                   @blur="$v.item.markup.$touch">
-          <div class="invalid-feedback" v-if="!$v.item.markup.required">
-            Markup field is required
-          </div>
-
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlTextarea1">Комментарий</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" v-model="item.comment"></textarea>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-2">
-                <label for="">Длина</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.length">
-            </div>
-            <div class="form-group col-md-2">
-                <label for="">Ширина </label>
-                <input type="text" class="form-control form-control-lg" v-model="item.width">
-            </div>
-            <div class="form-group col-md-2">
-                <label for="">Высота</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.height">
-            </div>
-            <div class="form-group col-md-3">
-                <label for="">Вес</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.weight">
-            </div>
-            <div class="form-group col-md-3">
-                <label for="">Цена</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.price">
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-4">
-                <label for="">Мета заголовок</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.meta_title">
-            </div>
-            <div class="form-group col-md-4">
-                <label for="">Мета описание </label>
-                <input type="text" class="form-control form-control-lg" v-model="item.meta_description">
-            </div>
-            <div class="form-group col-md-4">
-                <label for="">Мета ключевые слова</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.meta_keyword">
-            </div>
-        </div>
-        <button class="btn btn-primary" @click="create()">Создать</button>
+  <div>
+    <div class="form-group">
+      <label for="">Название *</label>
+      <input type="text" class="form-control form-control-lg" v-model="item.title"
+             :class="{'is-invalid': $v.item.title.$error}"
+             @blur="$v.item.title.$touch">
+      <div class="invalid-feedback" v-if="!$v.item.title.required">
+        Title field is required
+      </div>
     </div>
+    <div class="form-group">
+      <label for="">Наценка категории *</label>
+      <input type="text" class="form-control form-control-lg" v-model="item.markup"
+             :class="{'is-invalid': $v.item.markup.$error}"
+             @blur="$v.item.markup.$touch">
+      <div class="invalid-feedback" v-if="!$v.item.markup.required">
+        Markup field is required
+      </div>
+
+    </div>
+    <div class="form-group">
+      <label for="exampleFormControlTextarea1">Комментарий</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" v-model="item.comment"></textarea>
+    </div>
+    <div class="form-row">
+      <div class="form-group col-md-2">
+        <label for="">Длина</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.length">
+      </div>
+      <div class="form-group col-md-2">
+        <label for="">Ширина </label>
+        <input type="text" class="form-control form-control-lg" v-model="item.width">
+      </div>
+      <div class="form-group col-md-2">
+        <label for="">Высота</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.height">
+      </div>
+      <div class="form-group col-md-3">
+        <label for="">Вес</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.weight">
+      </div>
+      <div class="form-group col-md-3">
+        <label for="">Цена</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.price">
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="form-group col-md-4">
+        <label for="">Мета заголовок</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.meta_title">
+      </div>
+      <div class="form-group col-md-4">
+        <label for="">Мета описание </label>
+        <input type="text" class="form-control form-control-lg" v-model="item.meta_description">
+      </div>
+      <div class="form-group col-md-4">
+        <label for="">Мета ключевые слова</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.meta_keyword">
+      </div>
+    </div>
+    <button class="btn btn-primary" @click="create()">Создать</button>
+  </div>
 </template>
 
 <script>
@@ -68,27 +68,27 @@ import {mapActions} from "vuex";
 import {required} from "vuelidate/lib/validators";
 
 export default {
-    name: "CreateItem",
-    data() {
-        return {
-            item: {
-                price_id: 1,
-                title: '',
-                markup: '',
-                comment: '',
-                meta_title: '',
-                meta_description: '',
-                meta_keyword: '',
-                price: '',
-                width: '',
-                height: '',
-                length: '',
-                weight: ''
-            },
-            items: [],
-            prices: []
-        };
-    },
+  name: "CreateItem",
+  data() {
+    return {
+      item: {
+        price_id: 1,
+        title: '',
+        markup: '',
+        comment: '',
+        meta_title: '',
+        meta_description: '',
+        meta_keyword: '',
+        price: '',
+        width: '',
+        height: '',
+        length: '',
+        weight: ''
+      },
+      items: [],
+      prices: []
+    };
+  },
   validations: {
     item: {
       title: {
@@ -100,25 +100,25 @@ export default {
     }
   },
 
-    created() {
-        this.get()
+  created() {
+    this.get()
+  },
+  methods: {
+    ...mapActions(['getPrices', 'createItem']),
+    get() {
+      this.getPrices().then(res => {
+        this.prices = res.price
+      })
     },
-    methods: {
-        ...mapActions(['getPrices', 'createItem']),
-        get() {
-            this.getPrices().then(res => {
-                this.prices = res.price
-            })
-        },
-        create() {
-            this.createItem(this.item).then(res => {
-                if (res.success) {
-                    this.$router.push('/item/all')
-                } else {
-                    alert('Please check and insert again')
-                }
-            });
+    create() {
+      this.createItem(this.item).then(res => {
+        if (res.success) {
+          this.$router.push('/item/all')
+        } else {
+          alert('Please check and insert again')
         }
-    },
+      });
+    }
+  },
 };
 </script>
--- src/views/item/EditItem.vue
+++ src/views/item/EditItem.vue
@@ -1,133 +1,155 @@
 <template>
-    <div>
-<!--        <div class="form-group">-->
-<!--            <label for="">Цена </label>-->
-<!--            <select class="form-control form-control-lg" v-model="item.price_id">-->
-<!--                <option v-for="price in prices" :value="price.id">{{ price.id }}</option>-->
-<!--            </select>-->
-<!--        </div>-->
-        <div class="form-group">
-            <label >Название *</label>
-            <input type="text" class="form-control form-control-lg" name="title" v-model="item.title">
-        </div>
-        <div class="form-group">
-            <label >Наценка категории *</label>
-            <input type="text" class="form-control form-control-lg" name="markup" v-model="item.markup">
-        </div>
-        <div class="form-group">
-            <label >Комментарий</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
-                      v-model="item.comment"></textarea>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-2">
-                <label >Длина</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.length">
-            </div>
-            <div class="form-group col-md-2">
-                <label >Ширина </label>
-                <input type="text" class="form-control form-control-lg" v-model="item.width">
-            </div>
-            <div class="form-group col-md-2">
-                <label >Высота</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.height">
-            </div>
-            <div class="form-group col-md-3">
-                <label >Вес</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.weight">
-            </div>
-            <div class="form-group col-md-3">
-                <label >Цена</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.price">
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-4">
-                <label >Мета заголовок</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.meta_title">
-            </div>
-            <div class="form-group col-md-4">
-                <label >Мета описание </label>
-                <input type="text" class="form-control form-control-lg" v-model="item.meta_description">
-            </div>
-            <div class="form-group col-md-4">
-                <label >Мета ключевые слова</label>
-                <input type="text" class="form-control form-control-lg" v-model="item.meta_keyword">
-            </div>
-        </div>
-        <button class="btn btn-primary" @click="edit()">Изменить</button>
+  <div>
+    <!--        <div class="form-group">-->
+    <!--            <label for="">Цена </label>-->
+    <!--            <select class="form-control form-control-lg" v-model="item.price_id">-->
+    <!--                <option v-for="price in prices" :value="price.id">{{ price.id }}</option>-->
+    <!--            </select>-->
+    <!--        </div>-->
+    <div class="form-group">
+      <label for="">Название *</label>
+      <input type="text" class="form-control form-control-lg" v-model="item.title"
+             :class="{'is-invalid': $v.item.title.$error}"
+             @blur="$v.item.title.$touch">
+      <div class="invalid-feedback" v-if="!$v.item.title.required">
+        Title field is required
+      </div>
     </div>
+    <div class="form-group">
+      <label for="">Наценка категории *</label>
+      <input type="text" class="form-control form-control-lg" v-model="item.markup"
+             :class="{'is-invalid': $v.item.markup.$error}"
+             @blur="$v.item.markup.$touch">
+      <div class="invalid-feedback" v-if="!$v.item.markup.required">
+        Markup field is required
+      </div>
+
+    </div>
+    <div class="form-group">
+      <label>Комментарий</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                v-model="item.comment"></textarea>
+    </div>
+    <div class="form-row">
+      <div class="form-group col-md-2">
+        <label>Длина</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.length">
+      </div>
+      <div class="form-group col-md-2">
+        <label>Ширина </label>
+        <input type="text" class="form-control form-control-lg" v-model="item.width">
+      </div>
+      <div class="form-group col-md-2">
+        <label>Высота</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.height">
+      </div>
+      <div class="form-group col-md-3">
+        <label>Вес</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.weight">
+      </div>
+      <div class="form-group col-md-3">
+        <label>Цена</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.price">
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="form-group col-md-4">
+        <label>Мета заголовок</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.meta_title">
+      </div>
+      <div class="form-group col-md-4">
+        <label>Мета описание </label>
+        <input type="text" class="form-control form-control-lg" v-model="item.meta_description">
+      </div>
+      <div class="form-group col-md-4">
+        <label>Мета ключевые слова</label>
+        <input type="text" class="form-control form-control-lg" v-model="item.meta_keyword">
+      </div>
+    </div>
+    <button class="btn btn-primary" @click="edit()">Изменить</button>
+  </div>
 </template>
 
 <script>
 import {mapActions} from "vuex";
+import {required} from "vuelidate/lib/validators";
 
 export default {
-    name: "EditCategory",
-    data() {
-        return {
-            item: {
-                price_id: 1,
-                title: '',
-                markup: '',
-                comment: '',
-                meta_title: '',
-                meta_description: '',
-                meta_keyword: '',
-                price: '',
-                width: '',
-                height: '',
-                length:'',
-                weight:''
-            },
-            items: [],
-            prices: []
-        };
-    },
-    created() {
-        this.get()
-        if(this.$route.params.item){
-            this.item = this.$route.params.item
-            this.item.properties.forEach(item=>{
-                switch(item.name){
-                    case 'price':
-                        this.item.price =  item.value
-                        break
-                    case 'width':
-                        this.item.width =  item.value
-                        break
-                    case 'height':
-                        this.item.height =  item.value
-                        break
-                    case 'length':
-                        this.item.length =  item.value
-                        break
-                    case 'weight':
-                        this.item.weight =  item.value
-                        break
-                    default:
-                        break
-                }
-            })
-        }else{
-            this.$router.push('/item/all')
+  name: "EditCategory",
+  data() {
+    return {
+      item: {
+        price_id: 1,
+        title: '',
+        markup: '',
+        comment: '',
+        meta_title: '',
+        meta_description: '',
+        meta_keyword: '',
+        price: '',
+        width: '',
+        height: '',
+        length: '',
+        weight: ''
+      },
+      items: [],
+      prices: []
+    };
+  },
+  validations: {
+    item: {
+      title: {
+        required
+      },
+      markup: {
+        required
+      }
+    }
+  },
+  created() {
+    this.get()
+    if (this.$route.params.item) {
+      this.item = this.$route.params.item
+      this.item.properties.forEach(item => {
+        switch (item.name) {
+          case 'price':
+            this.item.price = item.value
+            break
+          case 'width':
+            this.item.width = item.value
+            break
+          case 'height':
+            this.item.height = item.value
+            break
+          case 'length':
+            this.item.length = item.value
+            break
+          case 'weight':
+            this.item.weight = item.value
+            break
+          default:
+            break
         }
+      })
+    } else {
+      this.$router.push('/item/all')
+    }
+  },
+  methods: {
+    ...mapActions(['getPrices', 'editItem']),
+    get() {
+      this.getPrices().then(res => {
+        this.prices = res.price
+      });
     },
-    methods: {
-        ...mapActions(['getPrices', 'editItem']),
-        get() {
-            this.getPrices().then(res => {
-                this.prices = res.price
-            });
-        },
-        edit() {
-            this.editItem(this.item).then(res => {
-                if (res.success) {
-                    this.$router.push('/item/all')
-                }
-            });
-        },
+    edit() {
+      this.editItem(this.item).then(res => {
+        if (res.success) {
+          this.$router.push('/item/all')
+        }
+      });
     },
+  },
 };
 </script>
 
